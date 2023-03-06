161f5a0beffe7d9ea9bde48e66a4659fda85d7e2
Marcos <marcos.moliveira@outlook.com>
2023-03-05 22:36:42 -0300

    add prisma e banco de dados

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,17 @@
 // para executar o comando no package.json basta colocar no terminal "npm run dev"
+import Fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
+
+const app = Fastify()
+const prisma = new PrismaClient()
+
+app.get('/', async ()=> {
+    const habits = await prisma.Habit.findMany()
+    return habits
+})
+
+app.listen({
+    port: 3333,
+}).then(()=> {
+    console.log('HTTP Server running!')
+})
