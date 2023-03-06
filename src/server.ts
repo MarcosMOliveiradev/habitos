4da276c1949877034135904ed9b0905cb83a7c8a
Marcos Suporte TI <suporte01@WENG02.INFO>
2023-03-06 14:18:02 -0300

    colocando cors

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,17 @@
 // para executar o comando no package.json basta colocar no terminal "npm run dev"
 import Fastify from 'fastify'
+import cors from '@fastify/cors'
 import { PrismaClient } from '@prisma/client'
 
 const app = Fastify()
 const prisma = new PrismaClient()
 
+app.register(cors)
+
 app.get('/', async ()=> {
     const habits = await prisma.Habit.findMany()
+
+    
     return habits
 })
 
